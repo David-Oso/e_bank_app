e58b1b5dd5ee266e404ac5dd079910a4584b438d
David <osodavid001@gmail.com>
2023-07-08 03:18:56 +0100

    done for the day july 7 2023

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/authController.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/authController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/auth/")
 @AllArgsConstructor
+
 public class authController {
     private final CustomerService customerService;
 //    private final AdminService adminService;
