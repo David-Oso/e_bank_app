fc711bc5ec832420fea6360369f0f157aa3104d2
David <osodavid001@gmail.com>
2023-07-08 03:16:20 +0100

    done with the customer controller

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/CustomerController.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.bank.E_Bank_App.controller;
 
 import com.bank.E_Bank_App.data.model.Customer;
+import com.bank.E_Bank_App.data.model.Transaction;
 import com.bank.E_Bank_App.dto.request.*;
 import com.bank.E_Bank_App.service.customer.CustomerService;
 import jakarta.validation.Valid;
@@ -9,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -34,55 +38,69 @@ public class CustomerController {
     }
     @PostMapping("set_up/account")
     public ResponseEntity<?> setUpAccount(@Valid @RequestBody SetUpAccountRequest setUpAccountRequest){
-        return null;
+        String setUpAccountResponse = customerService.setUpAccount(setUpAccountRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(setUpAccountResponse);
     }
     @PostMapping("deposit")
         public ResponseEntity<?> makeDeposit(@Valid @RequestBody DepositRequest depositRequest){
-        return null;
+        String depositResponse = customerService.makeDeposit(depositRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(depositResponse);
     }
     @PostMapping("withdraw")
     public ResponseEntity<?> makeWithdraw(@Valid @RequestBody WithDrawRequest withDrawRequest){
-        return null;
+        String withdrawResponse = customerService.makeWithdraw(withDrawRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(withdrawResponse);
     }
     @PostMapping("transfer")
     public ResponseEntity<?> makeTransfer(@Valid @RequestBody TransferRequest transferRequest){
-        return null;
+        String transferResponse = customerService.makeTransfer(transferRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(transferResponse);
     }
 
     @GetMapping("get/balance")
     public ResponseEntity<?> getBalance(@Valid @RequestParam Long customerId, @Valid @RequestParam String pin){
-        return null;
+        BigDecimal accountBalance = customerService.getBalance(customerId, pin);
+        return ResponseEntity.status(HttpStatus.OK).body(accountBalance);
     }
 
     @PutMapping("update")
     public ResponseEntity<?> updateCustomer(@Valid @RequestBody UpdateCustomerRequest updateCustomerRequest){
-        return null;
+        String updateCustomerResponse = customerService.updateCustomer(updateCustomerRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(updateCustomerResponse);
     }
     @PostMapping("reset/password")
     public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest){
-        return null;
+        String resetPasswordResponse = customerService.resetPassword(resetPasswordRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(resetPasswordResponse);
     }
     @PostMapping(value = "upload_image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> uploadImage(@Valid @ModelAttribute UploadImageRequest uploadImageRequest){
-        String response = customerService.uploadImage(uploadImageRequest);
-        return ResponseEntity.ok(response);
+        String uploadImageResponse = customerService.uploadImage(uploadImageRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(uploadImageResponse);
     }
     @GetMapping("get/transaction")
     public ResponseEntity<?> getTransactionByCustomerIdAndTransactionId(@Valid @RequestParam Long customerId, @Valid @RequestParam Long transactionId){
-        return null;
+        Transaction transaction = customerService
+                .getTransactionByCustomerIdAndTransactionId(customerId, transactionId);
+        return ResponseEntity.status(HttpStatus.OK).body(transaction);
     }
 
     @GetMapping("get/transactions/all/{customer_id}")
     public ResponseEntity<?> getAllTransactionsByCustomerId(@Valid @PathVariable Long customer_id){
-        return null;
+        List<Transaction> transactions = customerService.getAllTransactionsByCustomerId(customer_id);
+        return ResponseEntity.status(HttpStatus.OK).body(transactions);
     }
     @DeleteMapping("delete/transaction")
     public ResponseEntity<?> deleteTransactionByCustomerIdAndTransactionId(@Valid @RequestParam Long customerId, @Valid @RequestParam Long transactionId){
-        return null;
+        String deleteTransactionResponse = customerService
+                .deleteTransactionByCustomerIdAndTransactionId(customerId, transactionId);
+        return ResponseEntity.status(HttpStatus.OK).body(deleteTransactionResponse);
     }
     @DeleteMapping("delete/transaction/all/{customer_id}")
     public ResponseEntity<?> deleteAllTransactionsByCustomerId(@Valid @PathVariable Long customer_id){
-        return null;
+        String deleteAllTransactionResponse = customerService
+                .deleteAllTransactionsByCustomerId(customer_id);
+        return ResponseEntity.status(HttpStatus.OK).body(deleteAllTransactionResponse);
     }
 }
 
