1128f9efe33b92d9add107d7aefe05ef4c1cb445
David <osodavid001@gmail.com>
2023-07-08 02:28:14 +0100

    createdd the auth and customer controller

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/CustomerController.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/CustomerController.java
@@ -1,34 +1,83 @@
 package com.bank.E_Bank_App.controller;
 
+import com.bank.E_Bank_App.dto.request.*;
 import com.bank.E_Bank_App.service.customer.CustomerService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
 @AllArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
+
+    @GetMapping("get/{customer_id}")
+    public ResponseEntity<?> getCustomerById(@Valid @PathVariable Long customer_id){
+        return null;
+    }
+
+    @GetMapping("get/email")
+    public ResponseEntity<?> getCustomerByEmail(@Valid @RequestParam String email){
+        return null;
+    }
+    @GetMapping("get/account_number")
+    public ResponseEntity<?> getCustomerByAccountNumber(@Valid @RequestParam String accountNumber){
+        return null;
+    }
+    @PostMapping("set_up/account")
+    public ResponseEntity<?> setUpAccount(@Valid @RequestBody SetUpAccountRequest setUpAccountRequest){
+        return null;
+    }
+    @PostMapping("deposit")
+        public ResponseEntity<?> makeDeposit(@Valid @RequestBody DepositRequest depositRequest){
+        return null;
+    }
+    @PostMapping("withdraw")
+    public ResponseEntity<?> makeWithdraw(@Valid @RequestBody WithDrawRequest withDrawRequest){
+        return null;
+    }
+    @PostMapping("transfer")
+    public ResponseEntity<?> makeTransfer(@Valid @RequestBody TransferRequest transferRequest){
+        return null;
+    }
+
+    @GetMapping("get/balance")
+    public ResponseEntity<?> getBalance(@Valid @RequestParam Long customerId, @Valid @RequestParam String pin){
+        return null;
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<?> updateCustomer(@Valid @RequestBody UpdateCustomerRequest updateCustomerRequest){
+        return null;
+    }
+    @PostMapping("reset/password")
+    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest){
+        return null;
+    }
+    @PostMapping(value = "upload_image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadImage(@Valid @ModelAttribute UploadImageRequest uploadImageRequest){
+        String response = customerService.uploadImage(uploadImageRequest);
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("get/transaction")
+    public ResponseEntity<?> getTransactionByCustomerIdAndTransactionId(@Valid @RequestParam Long customerId, @Valid @RequestParam Long transactionId){
+        return null;
+    }
+
+    @GetMapping("get/transactions/all/{customer_id}")
+    public ResponseEntity<?> getAllTransactionsByCustomerId(@Valid @PathVariable Long customer_id){
+        return null;
+    }
+    @DeleteMapping("delete/transaction")
+    public ResponseEntity<?> deleteTransactionByCustomerIdAndTransactionId(@Valid @RequestParam Long customerId, @Valid @RequestParam Long transactionId){
+        return null;
+    }
+    @DeleteMapping("delete/transaction/all/{customer_id}")
+    public ResponseEntity<?> deleteAllTransactionsByCustomerId(@Valid @PathVariable Long customer_id){
+        return null;
+    }
 }
 
-// RegisterResponse register(RegisterRequest request);
-//    String verifyEmail(EmailVerificationRequest request);
-//    void resendVerificationMail(Customer customer);
-//    AuthenticationResponse authenticate(AuthenticationRequest request);
-//    Customer getCustomerById(Long customerId);
-//    Customer getCustomerByEmail(String email);
-//    Customer getCustomerByAccountNumber(String accountNumber);
-//    String setUpAccount(SetUpAccountRequest request);
-//    String makeDeposit(DepositRequest request);
-//    String makeWithdraw(WithDrawRequest request);
-//    String makeTransfer(TransferRequest request);
-//    BigDecimal getBalance(Long customerId, String pin);
-//    String updateCustomer(UpdateCustomerRequest request);
-//    String sendRequestPasswordMail(Long customerId);
-//    String resetPassword(ResetPasswordRequest request);
-//    String uploadImage(UploadImageRequest request);
-//    Transaction getTransactionByCustomerIdAndTransactionId(Long customerId, Long transactionId);
-//    List<Transaction> getAllTransactionsByCustomerId(Long customerId);
-//    String deleteTransactionByCustomerIdAndTransactionId(Long customerId, Long transactionId);
-//    String deleteAllTransactionsByCustomerId(Long customerId);
--- E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/authController.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/controller/authController.java
@@ -1,5 +1,6 @@
 package com.bank.E_Bank_App.controller;
 
+import com.bank.E_Bank_App.dto.request.AdminLoginRequest;
 import com.bank.E_Bank_App.dto.request.AuthenticationRequest;
 import com.bank.E_Bank_App.dto.request.EmailVerificationRequest;
 import com.bank.E_Bank_App.dto.request.RegisterRequest;
@@ -7,29 +8,42 @@ import com.bank.E_Bank_App.service.customer.CustomerService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/auth")
+@RequestMapping("api/v1/auth/")
 @AllArgsConstructor
 public class authController {
     private final CustomerService customerService;
 //    private final AdminService adminService;
 
 
-    @PostMapping("/user/register")
+    @PostMapping("user/register")
     public ResponseEntity<?> registerCustomer(@Valid @RequestBody RegisterRequest registerRequest){
         return null;
     }
-    @PostMapping("/verify")
+    @PostMapping("verify")
     public ResponseEntity<?> verify(@Valid @RequestBody EmailVerificationRequest emailVerificationRequest){
         return null;
     }
-    @PostMapping("/login")
+    @PostMapping("customer/login")
     public ResponseEntity<?>  customerLogin(@Valid @RequestBody AuthenticationRequest authenticationRequest){
+        return null;
+    }
+
+    @PostMapping("admin/login")
+    public ResponseEntity<?> adminLogin(@Valid @RequestBody AdminLoginRequest adminLoginRequest){
+        return null;
+    }
+
+
+    @GetMapping("send_verification_mail/{customer_id}")
+    public ResponseEntity<?> resendVerificationMail(@Valid @PathVariable Long customer_id){
+        return null;
+    }
+
+    @GetMapping("send_reset_password_mail/{customer_id}")
+    public ResponseEntity<?> sendResetPasswordMail(@Valid @PathVariable Long customer_id){
         return null;
     }
 
