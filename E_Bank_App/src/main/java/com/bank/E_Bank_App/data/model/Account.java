eb4df40e9fc4e97c4af187b6a6db250d5417794a
David <osodavid001@gmail.com>
2023-09-19 08:51:49 +0100

    tuesday

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/data/model/Account.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/data/model/Account.java
@@ -23,4 +23,5 @@ public class Account {
     private String pin;
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     private List<Transaction> transactions;
+    private String mende;
 }
