f299d87a499aa333a55d35e96ccf56707ec5fc88
David <osodavid001@gmail.com>
2023-09-22 06:52:57 +0100

    app userf

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/data/repository/AppUserRepository.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/data/repository/AppUserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findAppUserByEmail(String email);
+    AppUser findAppUserByFirstName(String firstName);
 }
