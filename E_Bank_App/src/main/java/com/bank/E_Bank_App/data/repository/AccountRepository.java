2ee5fcf504b39935680e74a2d4f2c9d6325bcc19
David <osodavid001@gmail.com>
2023-09-22 06:49:40 +0100

    accountf

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/data/repository/AccountRepository.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/data/repository/AccountRepository.java
@@ -4,4 +4,5 @@ import com.bank.E_Bank_App.data.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findAccountByAccountName(String name);
 }
