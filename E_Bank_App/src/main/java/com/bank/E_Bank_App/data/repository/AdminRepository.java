96b79eb7f7335bf52a3a56bc98897df495569998
David <osodavid001@gmail.com>
2023-09-25 08:39:24 +0100

    clear password

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/data/repository/AdminRepository.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/data/repository/AdminRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findAdminByAppUser_Email(String email);
-String password;
 
 }
