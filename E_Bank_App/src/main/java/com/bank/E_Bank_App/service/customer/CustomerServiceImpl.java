24f99d16e46d3192f3b9de1a78db20076878d4aa
David <osodavid001@gmail.com>
2023-09-27 09:24:39 +0100

    holidaye

--- E_Bank_App/src/main/java/com/bank/E_Bank_App/service/customer/CustomerServiceImpl.java
+++ E_Bank_App/src/main/java/com/bank/E_Bank_App/service/customer/CustomerServiceImpl.java
@@ -465,6 +465,7 @@ private void dateOfBirth(Long userId, String dateOfBirth){
             if (transaction.getId().equals(transactionId))
                 return transaction;
         }
+
         throw new E_BankException("Transaction not found");
     }
 
